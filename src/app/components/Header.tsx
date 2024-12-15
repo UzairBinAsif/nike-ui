9fadc14eb54bbe61be5f76cc8adc61d4406215c0
UZAIR BIN ASIF <160782214+UzairBinAsif@users.noreply.github.com>
2024-12-15 13:35:26 +0500

    update src & public 0.1.2

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React from 'react'
 import Image from 'next/image'
 import Logo from '@/../public/mainLogoVector.svg'
@@ -5,35 +7,42 @@ import heart from '@/../public/heart.svg'
 import bag from '@/../public/bag.svg'
 import search from '@/../public/search.svg'
 import Link from 'next/link'
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from 'react'
 
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
-    <div className='flex justify-center items-center w-screen h-[60px] bg-[#FFFFFF]'>
+    <div className='flex justify-center items-center w-screen h-[7vh] px-[3vw] lg-px-0 bg-[#FFFFFF]'>
       <div>
         <Image src={Logo} alt='Logo Image' width={58.85}/>
       </div>
       <div className='flex items-center justify-end w-[80vw] h-full'>
-        <div className='flex items-center justify-around w-[60%] h-full text-[#111111] text-[16px] font-medium  leading-[24px]'>
-          <Link href={'/'}>New & Featured</Link>
-          <Link href={'/'}>Men</Link>
-          <Link href={'/'}>Women</Link>
-          <Link href={'/'}>Kids</Link>
-          <Link href={'/'}>Sale</Link>
-          <Link href={'/'}>SNKRS</Link>
+        <div className='hidden md:flex items-center justify-around w-[60%] h-full text-[#111111] text-[14px] md:text-[16px] font-medium  leading-[24px]'>
+          <Link className='nav-links hover:text-gray-500 transition ease-in-out' href={'/'}>New & Featured</Link>
+          <Link className='nav-links hover:text-gray-500 transition ease-in-out' href={'/'}>Men</Link>
+          <Link className='nav-links hover:text-gray-500 transition ease-in-out' href={'/'}>Women</Link>
+          <Link className='nav-links hover:text-gray-500 transition ease-in-out' href={'/'}>Kids</Link>
+          <Link className='nav-links hover:text-gray-500 transition ease-in-out' href={'/'}>Sale</Link>
+          <Link className='nav-links hover:text-gray-500 transition ease-in-out' href={'/'}>SNKRS</Link>
         </div>
-        <div className='ml-[50px] mr-[20px] flex items-center w-[180px] h-[40px] rounded-[100px] bg-[#F5F5F5]'>
-            <div className='flex items-center justify-center w-[40px] h-[36px]'>
+        <div className='ml-[50px] mr-[20px] flex items-center w-[160px] md:w-[180px] h-[40px] rounded-[100px] bg-[#F5F5F5]'>
+            <div className='flex items-center justify-center w-[40px] h-[36px] hover:opacity-60 cursor-pointer'>
                 <Image src={search} alt='image'/>
             </div>
-            <input type='text' placeholder='Search' className='flex items-center w-[84px] h-[18px] text-[#CCCCCC] font-medium text-[15px] leading-[18.31px]'></input>
+            <input type='text' placeholder='Search' className='bg-[#F5F5F5] flex items-center w-[84px] h-[18px] text-[black] font-medium text-[15px] leading-[18.31px]'></input>
         </div>
       </div>
       <div className='flex justify-center items-center w-[84px] h-full'>
-        <div className='flex justify-center items-center w-9 h-9'>
+        <div className='flex justify-center items-center w-9 h-9 hover:opacity-60 cursor-pointer'>
         <Image src={heart} alt='Logo Image'/>
         </div>
-        <div className='flex justify-center items-center w-9 h-9'>
+        <div className='flex justify-center items-center w-9 h-9 hover:opacity-60 cursor-pointer'>
         <Image src={bag} alt='Logo Image'/>
         </div>
       </div>
--- src/app/components/TopHeader.tsx
+++ src/app/components/TopHeader.tsx
@@ -4,24 +4,24 @@ import logo from "@/../public/Vector.svg";
 
 const TopHeader = () => {
   return (
-    <div className="flex justify-center w-screen h-[36px] bg-[#F5F5F5]">
+    <div className="flex justify-center w-screen h-[5vh] py-0 bg-[#F5F5F5]">
       <div className="flex items-center justify-between w-[93vw]">
-        <div className=" w-6 h-6">
+        <div className="ml-[1vw] w-5 h-5 md:w-6 md:h-6">
           <Image src={logo} alt="Image" width={24} />
         </div>
         <div className="flex w-[272.81px] font-medium text-[11px] leading-[14px] h-full">
             <div className="flex justify-around items-center w-[98.5px] h-full">
-                <div className="cursor-pointer">Find a Store</div>
+                <div className="cursor-pointer hover:text-gray-500 transition ease-in-out">Find a Store</div>
                 <div>|</div>
             </div>
             <div className="flex justify-around items-center w-[51.81px] h-full">
-                <div className="cursor-pointer">Help</div>
+                <div className="cursor-pointer hover:text-gray-500 transition ease-in-out">Help</div>
                 <div>|</div>
             </div>
             <div className="flex justify-around items-center w-[122.5px] h-full">
-                <div className="cursor-pointer">Join Us</div>
+                <div className="cursor-pointer hover:text-gray-500 transition ease-in-out">Join Us</div>
                 <div>|</div>
-                <div className="cursor-pointer">Sign In</div>
+                <div className="cursor-pointer hover:text-gray-500 transition ease-in-out">Sign In</div>
             </div>
         </div>
       </div>
